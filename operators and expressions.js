7a65b5b5c4e52e9d7b4237ed1d09a2400a23ff94
SYN <SYN@mail.com>
2023-01-05 14:23:09 +0530

    created operators and expressions file and witten all data

---
+++ operators and expressions.js
@@ -0,0 +1,49 @@
+//  A fragment of value that produces and code is an expression.
+
+// example |>
+
+// 77;
+// "syn";
+// true;
+
+// opreaters in js |>
+
+console.log("Operators in js : ");
+
+let a = 10;
+
+console.log(a);
+
+a++; // js have a++ and ++a |> ++a first increments the value and then prints on the other hand a++ firstly prints the value and then increaments.
+
+console.log(a);
+
+// also js have a-- and --a.
+
+let b = 11;
+
+console.log(b);
+
+--a;
+
+console.log(b);
+
+// also js have ** and // and mod (%) operators
+
+// js have all the assignment opreater that all programming laguages use
+
+// exmaple |>
+// += , -=, /= , *=
+
+// comparsion operators |>
+
+let comp1 = 5;
+let comp2 = "10";
+// the difference b/w double equals to and triple equals to is == compares only the values, on the other hand triple equals to === compares values and value types both.
+console.log(comp1 == comp2);
+console.log(comp1 === comp2);
+
+
+// logical operators |>
+
+// js have all logical operators that are logcal not, logical or and logical and like other programming languages. 
