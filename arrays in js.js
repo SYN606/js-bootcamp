6485201894d37192fddd359a69149e0a734146f2
SYN <SYN@mail.com>
2023-01-10 22:35:14 +0530

    added more array methods

--- arrays in js.js
+++ arrays in js.js
@@ -71,8 +71,60 @@ let s = num.shift();
 
 let un = num.unshift(35);
 
-console.log(s);
-console.log(un);
+// console.log(s);
+// console.log(un);
+
+// console.log(num);
 
 
-console.log(num);
+// using delete function 
+
+// console.log(num.length);
+
+// delete num[0]; // the delete funtion doesnt affects on the arrays length the leaves it blank and the length of an array remains same.
+
+// console.log(num);
+
+
+// concatenating the two arrays 
+
+let num_more = [72, 55, 26, 66, 14, 25, 48];
+
+let newArray = num.concat(num_more); // by using this we can cancatenate multiple arrays and it does not changes the existence of an ld array.
+
+// console.log(newArray);
+
+
+// sorting the arrays 
+
+let sorted_array = newArray.sort();
+
+// console.log(sorted_array);
+
+// this sorts an array and changes the original array. Sort method sorts all elements alphabetically. Sort have a compare function also.
+
+
+// reversing an array 
+
+let reverse_array = newArray.reverse();
+
+// console.log(reverse_array); // this will change the original array.
+console.log(newArray);
+
+
+// splice
+
+// syntax |> array.splice(start_index, number of elements, values)
+
+// newArray.splice(2, 4, 1024, 1025, 1026, 1027);
+// console.log(newArray);
+
+// slice
+
+// syntax |> array.slice(start_index, end_index{ last index is not included})
+
+let sliced_array = newArray.slice(6);
+let new_sliced_array = newArray.slice(2, 7);
+
+console.log(sliced_array);
+console.log(new_sliced_array);
