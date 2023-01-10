cc3fca74b45bc77c0a46444ceed35deb04c3419c
SYN <SYN@mail.com>
2023-01-10 21:51:22 +0530

    added arrays file in js

---
+++ arrays in js.js
@@ -0,0 +1,28 @@
+
+// This file contains array and array methods
+
+let a = [1, 2, "Not Given", 3, 4, 5];
+
+// console.log(a);
+
+// |> accessing values from arrays
+
+// console.log(a[0]); //  this will only prnt the value of a[0's_element]. this can be used for all indexes. the index always starts from the 0.
+
+// console.log(a[7]); // this will print undefined because the index 7 isn't present in the array a.
+
+// printing the list of a list 
+
+// console.log(a.length); // this is print the total numbers of items present in list.
+
+// adding a new value to the array
+
+a[8] = 45;
+
+// console.log(a[8]);
+// also using this way we can change the values of a list.
+
+a[2] = "Given";
+
+// console.log(a[2]);
+
